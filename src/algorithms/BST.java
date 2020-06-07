6cc60ff687f44bf4faddad021b19cbc36f8e2683
Benny Fellman <benny.fellman@gmail.com>
2020-06-07 19:02:26 +0300

    lazy list impl

--- src/algorithms/BST.java
+++ src/algorithms/BST.java
@@ -8,22 +8,77 @@ import java.util.HashSet;
 
 public class BST implements BSTInterface {
 
-    private final HashSet<Long> setA;
+    class Node {
+        final int key; // key is immutable
+        Node next;
+        boolean marked;
 
+        public Node(int key) { // Node constructor
+            this.key = key;
+            this.next = null;
+            this.marked = false;
+        }
+    }
+    final Node head;
     public BST() {
-        setA = new HashSet<Long>();
+        head = new Node(Integer.MIN_VALUE);
+        head.next = new Node(Integer.MAX_VALUE);
     }
 
+
     public final boolean contains(final int key) {
-        return setA.contains((long)key);
+        Node curr = head;
+        while (curr.key < key)
+            curr = curr.next;
+        return curr.key == key && !curr.marked;
     }
 
     public final boolean insert(final int key) {
-        return setA.add((long)key);
+        while (true) {
+            Node pred = head;
+            Node curr = pred.next;
+            while (curr.key < key) {
+                pred = curr; curr = curr.next;
+            }
+            synchronized(pred) {
+                synchronized(curr) {
+                    if (validate (pred, curr)) {
+                        if (curr.key == key) {
+                            return false;
+                        } else {
+                            Node node = new Node(key);
+                            node.next = curr;
+                            pred.next = node;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public final boolean remove(final int key) {
-        return setA.remove((long)key);
+
+        while (true) {
+            Node pred = head;
+            Node curr = pred.next;
+            while (curr.key < key) {
+                pred = curr; curr = curr.next;
+            }
+            synchronized(pred) {
+                synchronized(curr) {
+                    if (validate (pred, curr)) {
+                        if (curr.key != key) {
+                            return false;
+                        } else {
+                            curr.marked = true;
+                            pred.next = curr.next;
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     // Return your ID #
@@ -36,21 +91,28 @@ public class BST implements BSTInterface {
     // NOTE: Guaranteed to be called without concurrent operations,
 	// so need to be thread-safe.  The method will only be called
 	// once the benchmark completes.
-        return setA.size();
+        Node iter = head;
+        int count = 0;
+        while(iter.next != null)
+            count++;
+        return count;
     }
 
     // Returns the sum of keys in the tree
     public final long getKeysum() {
+
     // NOTE: Guaranteed to be called without concurrent operations,
 	// so no need to be thread-safe.
 	//
 	// Make sure to sum over a "long" variable or you will get incorrect
 	// results due to integer overflow!
-        long sum=0;
-        Iterator<Long> iter = setA.iterator();
-        while(iter.hasNext()){
-            sum = sum + iter.next();
-        }
-    return sum;
+        Node iter = head;
+        long sum = 0;
+        while(iter.next != null)
+            sum += iter.key;
+        return sum;
+    }
+    boolean validate(Node pred, Node curr) {
+        return !pred.marked && !curr.marked && pred.next == curr;
     }
 }
